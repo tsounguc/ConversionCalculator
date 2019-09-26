5d4f36b40a0f5f05daf202d0f699b834a251d855
Christian Tsoungui Nkoulou <tsounguc@mail.gvsu.edu>
2019-09-26 05:44:03 -0400

    last commit

--- Hw3/UnitsAndModes.swift
+++ Hw3/UnitsAndModes.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Jonathan Engelsma. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 enum CalculatorMode : String {
     case Length
@@ -64,7 +64,7 @@ let volumeConversionTable : Dictionary<VolumeConversionKey, Double> = [
     VolumeConversionKey(toUnits: .Quarts, fromUnits: .Quarts) : 1.0
 ]
 
-// To support Swift 4.2's iteration over enum... see
+// To support Swift 4.2's iteration over enum... see 
 // source: https://stackoverflow.com/questions/24007461/how-to-enumerate-an-enum-with-string-type
 #if !swift(>=4.2)
 public protocol CaseIterable {
--- Hw3/ViewController.swift
+++ Hw3/ViewController.swift
@@ -10,23 +10,35 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var yardsField: UITextField!
-    @IBOutlet weak var metersField: UITextField!
+    @IBOutlet weak var FromTextField: DecimalMinusTextField!
+    
+    @IBOutlet weak var ToTextFied: DecimalMinusTextField!
+    @IBOutlet weak var FromLabel: UILabel!
+    @IBOutlet weak var ToLabel: UILabel!
+    @IBOutlet weak var TitleLabel: UILabel!
+    var calculatorMode: CalculatorMode = .Length
+    var fromLengthUnits: LengthUnit = LengthUnit.Meters
+    var toLengthUnits: LengthUnit = LengthUnit.Yards
+    var fromVolumeUnits: VolumeUnit = VolumeUnit.Liters
+    var toVolumeUnits: VolumeUnit = VolumeUnit.Gallons
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
             self.view.addGestureRecognizer(detectTouch)
+        self.title = "Length Conversion Calculator"
     }
     
     
     @IBAction func yardFieldDidBeginEditing(_ sender: UITextField) {
-        metersField.text = ""
+        ToTextFied.text = ""
     }
     
     
     @IBAction func meterFieldDidBeginEditing(_ sender: UITextField) {
-        yardsField.text = ""
+        FromTextField.text = ""
     }
     
     
@@ -35,28 +47,49 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func clearButtonTapped(_ sender: UIButton) {
-        yardsField.text = ""
-        metersField.text = ""
+        FromTextField.text = ""
+        ToTextFied.text = ""
     }
+    
+    
+    @IBAction func modeButtonTapped(_ sender: UIButton) {
+        switch calculatorMode {
+    case .Volume:
+        calculatorMode = .Length
+        FromTextField.placeholder = "Enter length in \(fromLengthUnits.rawValue)"
+        ToTextFied.placeholder = "Enter length in \(toLengthUnits.rawValue)"
+        TitleLabel.text = "Length Conversion Calculator"
+        FromLabel.text = fromVolumeUnits.rawValue
+        ToLabel.text = toVolumeUnits.rawValue
+    case .Length:
+        calculatorMode = .Volume
+        FromTextField.placeholder = "Enter volume in \(fromVolumeUnits.rawValue)"
+        ToTextFied.placeholder = "Enter volume in \(toVolumeUnits.rawValue)"
+        TitleLabel.text = "Volume Conversion Calculator"
+        FromLabel.text = fromLengthUnits.rawValue
+        ToLabel.text = toLengthUnits.rawValue
+           
+        
+        
+        }
+    }
+    
     @IBAction func calculateButtonTapped(_ sender: UIButton) {
-        if let y = self.yardsField.text, let m = metersField.text{
+        if let y = self.FromTextField.text, let m = ToTextFied.text{
             var result: Double
             if y != "" {
                 result = Double(y)! / 1.0936
-                self.metersField.text = "\(result)"
+                self.ToTextFied.text = "\(result)"
             }
             else if m != "" {
                 result = Double(m)! * 1.0936
-                self.yardsField.text = "\(result)"
+                self.FromTextField.text = "\(result)"
             }
             
         }
         self.dismissKeyboard()
     }		
     
-    @IBAction func modeButtonTapped(_ sender: UIButton) {
-         
-    }
     
 }
 
