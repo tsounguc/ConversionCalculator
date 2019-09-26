99f0ea04f4a5d92f816b0b5286441f0ef1aa9c39
Christian Tsoungui Nkoulou <tsounguc@mail.gvsu.edu>
2019-09-26 00:34:26 -0400

    new commit

---
+++ Hw3/ModeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ModeViewController.swift
+//  Hw3
+//
+//  Created by user158049 on 9/26/19.
+//  Copyright © 2019 Christian Tsoungui Nkoulou. All rights reserved.
+//
+
+import UIKit
+
+class ModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Hw3/ViewController.swift
+++ Hw3/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var yardsField: UITextField!
+    @IBOutlet weak var metersField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+            self.view.addGestureRecognizer(detectTouch)
     }
-
-
+    
+    
+    @IBAction func yardFieldDidBeginEditing(_ sender: UITextField) {
+        metersField.text = ""
+    }
+    
+    
+    @IBAction func meterFieldDidBeginEditing(_ sender: UITextField) {
+        yardsField.text = ""
+    }
+    
+    
+    @objc func dismissKeyboard(){
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+        yardsField.text = ""
+        metersField.text = ""
+    }
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        if let y = self.yardsField.text, let m = metersField.text{
+            var result: Double
+            if y != "" {
+                result = Double(y)! / 1.0936
+                self.metersField.text = "\(result)"
+            }
+            else if m != "" {
+                result = Double(m)! * 1.0936
+                self.yardsField.text = "\(result)"
+            }
+            
+        }
+        self.dismissKeyboard()
+    }		
+    
+    @IBAction func modeButtonTapped(_ sender: UIButton) {
+         
+    }
+    
 }
 
